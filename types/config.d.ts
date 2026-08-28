ce7b2e0859193bdc8c81c809d4056298ec68722c
Daniel <845765@qq.com>
2026-08-28 12:39:13 +0800

    :sparkles: Add cursor surrounding lines configuration type https://github.com/siyuan-note/siyuan/issues/19003

--- types/config.d.ts
+++ types/config.d.ts
@@ -608,6 +608,10 @@ export namespace Config {
          */
         fontSize: number;
         /**
+         * Minimum number of visible lines around the cursor during vertical keyboard navigation
+         */
+        cursorSurroundingLines: number;
+        /**
          * Whether the editor uses maximum width
          */
         fullWidth: boolean;
